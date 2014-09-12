d377366a2aecdf04115c041890a4bdb17a111b07
Jakob Skov <jakob123@gmail.com>
2014-09-12 23:34:34 +0200

    Ting er blevet inddelt i mapper. AlienInstantiation scenen er blevet tilføjet. Deri spawner aliens nu kun ved hjælp af koden.

---
+++ SpaceInvaders/Assets/Resources/Scripts/AlienInstantiation.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienInstantiation : MonoBehaviour {
+
+	public GameObject gameObject = null;
+
+	// Use this for initialization
+	void Start () {
+		int i = 0;
+		int posX = 0;
+		bool isPosXReset = false;
+		while (i < 6) {
+			gameObject = Instantiate(Resources.Load("Prefabs/Alien"), new Vector3(posX, 0, 0), Quaternion.identity) as GameObject;
+			i++;
+			posX += 2;
+		}
+		while (i >= 6 && i < 12) {
+			if (isPosXReset == false)  {
+				posX = 0;
+				isPosXReset = true;
+			}
+			gameObject = Instantiate(Resources.Load("Prefabs/Alien"), new Vector3(posX, 2, 0), Quaternion.identity) as GameObject;
+			i++;
+			posX += 2;
+		}
+		while (i >= 12 && i < 18) {
+			if (isPosXReset == true)  {
+				posX = 0;
+				isPosXReset = false;
+			}
+			gameObject = Instantiate(Resources.Load("Prefabs/Alien"), new Vector3(posX, 4, 0), Quaternion.identity) as GameObject;
+			i++;
+			posX += 2;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
